d4fba388a68fcede4fe97a3183a0ef306fdbf4b5
Andrey Danilov <andrewdanilov@gmail.com>
2019-03-18 23:49:56 +0300

    frontend images uploader

---
+++ src/Bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace andrewdanilov\InputImages;
+
+use yii\base\BootstrapInterface;
+
+class Bootstrap implements BootstrapInterface
+{
+	public function bootstrap($app)
+	{
+		$app->getUrlManager()->addRules([
+			[
+				'class' => 'andrewdanilov\InputImages\UploadUrlRule',
+			],
+		]);
+		$app->controllerMap['upload'] = [
+			'class' => 'andrewdanilov\inputImages\uploadController',
+		];
+	}
+}
---
+++ src/UploadUrlRule.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace andrewdanilov\InputImages;
+
+use yii\web\UrlRuleInterface;
+use yii\base\BaseObject;
+
+class UploadUrlRule extends BaseObject implements UrlRuleInterface
+{
+	public function createUrl($manager, $route, $params)
+	{
+		if ($route === 'upload/upload-image') {
+			if (isset($params['formId'])) {
+				return 'upload/upload-image/' . $params['formId'];
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param \yii\web\UrlManager $manager
+	 * @param \yii\web\Request $request
+	 * @return array|bool
+	 * @throws \yii\base\InvalidConfigException
+	 */
+	public function parseRequest($manager, $request)
+	{
+		$pathInfo = $request->getPathInfo();
+		if (preg_match('%^upload\/upload-image/(\w+)$%', $pathInfo, $matches)) {
+			$params['formId'] = $matches[1];
+			return ['upload/upload-image', $params];
+		}
+		return false;
+	}
+}
