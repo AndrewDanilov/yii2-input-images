b74f55c8b24dddc36980aa95d98935ee3530d0e4
Andrey Danilov <andrewdanilov@gmail.com>
2019-03-18 17:55:52 +0300

    frontend images uploader

--- src/web/js/input-images.js
+++ src/web/js/input-images.js
@@ -1,3 +1,17 @@
+if (typeof andrewdanilov === "undefined" || !andrewdanilov) {
+	var andrewdanilov = {};
+}
+
+andrewdanilov.inputImages = {
+	functions: {},
+	register: function(id, func) {
+		this.functions[id] = func;
+	},
+	callFunction: function(id, response) {
+		return this.functions[id](response, id);
+	}
+};
+
 /**
  * @return {boolean}
  */
