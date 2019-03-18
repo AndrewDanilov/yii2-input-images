5825eee24c37d7e6598c9e88eb820b353e751957
Andrey Danilov <andrewdanilov@gmail.com>
2019-03-19 01:14:36 +0300

    frontend images uploader

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -1,7 +1,12 @@
+<?php
+
+use yii\helpers\Json;
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<script type="text/javascript">window.parent.andrewdanilov.inputImages.callFunction(<?= $this->params['id']; ?>, <?= $this->params['response']; ?>);</script>
+	<script type="text/javascript">window.parent.andrewdanilov.inputImages.callFunction('<?= $this->params['id']; ?>', <?= JSON::encode($this->params['response']); ?>);</script>
 </head>
 <body>
 </body>
