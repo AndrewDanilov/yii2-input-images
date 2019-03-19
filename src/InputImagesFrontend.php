dd581a3768832d718f43ac5fbcf5d46ab6a46570
Andrey Danilov <andrewdanilov@gmail.com>
2019-03-19 16:02:09 +0300

    frontend images uploader

--- src/InputImagesFrontend.php
+++ src/InputImagesFrontend.php
@@ -58,7 +58,7 @@ class InputImagesFrontend extends InputWidget
 			<iframe name="upload_frame_' . $this->options['id'] . '" class="hidden-frame"></iframe>\
 			<form id="upload_form_' . $this->options['id'] . '" action="' . Url::to([$this->uploadHandler, 'formId' => $this->options['id']]) . '" target="upload_frame_' . $this->options['id'] . '" method="POST" enctype="multipart/form-data" class="hidden-form">\
 				<input type="hidden" name="' . Yii::$app->getRequest()->csrfParam . '" value="' . Yii::$app->getRequest()->getCsrfToken() . '" />\
-				<input type="file" name="file" id="upload_input_' . $this->options['id'] . '" accept="image/jpeg,image/png,image/gif" />\
+				<input type="file" name="image" id="upload_input_' . $this->options['id'] . '" accept="image/jpeg,image/png,image/gif" />\
 			</form>';
 		$this->getView()->registerJs("$('body').append('" . $uploadForm . "')");
 		$this->getView()->registerJs("$('body').on('change', '#upload_input_" . $this->options['id'] . "', function() { $(this).parent('form').submit(); });");
