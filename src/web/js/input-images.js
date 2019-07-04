c60be1cb7aff623b3a99942b61f89a2c48b01250
andrey <6097033725>
2019-07-04 21:06:01 +0300

    misc

--- src/web/js/input-images.js
+++ src/web/js/input-images.js
@@ -12,6 +12,22 @@ andrewdanilov.inputImages = {
 	}
 };
 
+andrewdanilov.getInputName = function(formName, formAttribute, multiple) {
+	var matches = formAttribute.match(/(^|.*\])([\w\.\+]+)(\[.*|$)/);
+	var prefix = matches[1];
+	var attribute = matches[2];
+	var suffix = matches[3];
+	if (multiple) {
+		suffix += '[]';
+	}
+	if (formName === '' && prefix === '') {
+		return attribute + suffix;
+	} else if (formName !== '') {
+		return formName + prefix + '[' + attribute + ']' + suffix;
+	}
+	return '';
+};
+
 /**
  * @return {boolean}
  */
@@ -20,7 +36,7 @@ var InputImagesHandler = function(files, id) {
 	var formName = wrapper.attr('data-form-name');
 	var formAttribute = wrapper.attr('data-form-attribute');
 	var items = wrapper.find('.input-images-items');
-	var input = '<input type="hidden" name="' + formName + '[' + formAttribute + '][]">';
+	var input = '<input type="hidden" name="' + andrewdanilov.getInputName(formName, formAttribute, true) + '">';
 	for (var i in files) {
 		var newItem = $('<div class="input-images-item"><div class="input-images-preview"><img src="" alt="" /></div>' + input + '<div class="input-images-remove fa fa-times"></div></div>');
 		if (files.hasOwnProperty(i)) {
@@ -40,7 +56,7 @@ var InputImageHandler = function(file, id) {
 	var formName = wrapper.attr('data-form-name');
 	var formAttribute = wrapper.attr('data-form-attribute');
 	var items = wrapper.find('.input-images-items');
-	var input = '<input type="hidden" name="' + formName + '[' + formAttribute + ']">';
+	var input = '<input type="hidden" name="' + andrewdanilov.getInputName(formName, formAttribute, false) + '">';
 	var newItem = $('<div class="input-images-item"><div class="input-images-preview"><img src="" alt="" /></div>' + input + '<div class="input-images-remove fa fa-times"></div></div>');
 	newItem.find('img').attr('src', file.url);
 	newItem.find('input').val(file.url);
